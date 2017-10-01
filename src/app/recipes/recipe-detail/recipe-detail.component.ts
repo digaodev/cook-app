ae1161299fa59338b0c241bfbd4cb665aff28c18
digaodev <rodrigo.rb.dev@gmail.com>
2017-10-01 18:17:37 -0300

    feature: add dropdown directive to header and recipe detail

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Recipe } from './../../shared/recipe.model';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-detail',
