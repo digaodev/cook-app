6771c7bca5d89ea6221b79235a10094e0130302e
digaodev <rodrigo.rb.dev@gmail.com>
2017-10-17 12:07:44 -0200

    Refactor: restructure Home and Header components to the Core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,33 @@ import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 
+import { AuthGuard } from './auth/auth-guard.service';
+import { AuthService } from './auth/auth.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
 
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule, // common module feats + other stuff
-    AppRoutingModule,
     HttpModule,
-    ShoppingListModule,
+    AppRoutingModule,
     AuthModule,
-    SharedModule
+    SharedModule,
+    CoreModule,
+    ShoppingListModule
   ],
   providers: [
     ShoppingListService,
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,24 @@
+import { AppRoutingModule } from './../app-routing.module';
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../shared/shared.module';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    HomeComponent
+  ],
+  imports: [
+    SharedModule,
+    AppRoutingModule
+  ],
+  exports: [
+    AppRoutingModule,
+    HeaderComponent
+  ]
+})
+export class CoreModule {
+
+}
